c05fae921a02b17e20f2bdd55b556d4afd87a08a
AnthonyBerisha <berishaanthony@gmail.com>
2023-08-29 15:50:25 +0200

    Rendering templates + wip post decal form

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "home"
+    return render_template("home.html")
 
 @app.route("/post")
 def post():
-    return "post page"
+    return render_template("post.html")
